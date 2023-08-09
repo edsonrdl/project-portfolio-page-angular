e8136e184aa1ceb40cab92cf82f6c58fd77bbd88
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-08-08 21:46:19 -0300

    feat: Criado json javaScript

--- src/app/view/components/model-list/model-list-academic.ts
+++ src/app/view/components/model-list/model-list-academic.ts
@@ -285,6 +285,472 @@ export const ModelListAcademic = [
           },
         ],
       },
+      {
+        label: 'CSS 3',
+        children: [
+          {
+            label: 'Sintaxe e estrutura básica de CSS',
+            children: [
+              { label: 'Seletores CSS ' },
+              { label: 'Propriedades CSS' },
+              { label: 'modelo de caixa' },
+            ],
+          },
+          {
+            label: 'Layouts com CSS',
+            children: [
+              { label: 'Posicionamento' },
+              { label: 'Flex box' },
+              { label: 'Grid' },
+              { label: 'Design web responsivo' },
+              { label: 'Float ' },
+              { label: 'Media queries' },
+            ],
+          },
+          {
+            label: 'Tipografia',
+            children: [
+              {label: 'Fontes e tipos de fontes'},
+              {label: 'Espaçamento entre linhas' },
+              {label: 'Tamanhos e unidades de medidas' },
+            ],
+          },
+          {
+            label: 'Cores e gradientes',
+            children: [
+              { label: 'Núcleos em CSS' },
+              { label: 'Gradientes lineares e radiais' },
+              { label: 'Transparência e opacidade' },
+              { label: 'Box model' },
+            ],
+          },
+          {
+            label: 'Efeitos e animações',
+            children: [
+            { label: 'Transições CSS ' },
+            { label: 'Transformações CSS ' },
+            { label: 'Animações CSS ' },
+           ],
+          },
+          {
+            label: 'CSS avançado',
+            children: [
+            { label: 'Pré-proecessadores ' },
+            { label: 'Consultas de mídia ' },
+            { label: 'Pseudoclasses e pseudoelementos ' },
+           ],
+          },
+          {
+            label: 'Frameworks e bibliotecas CSS',
+            children: [
+            { label: 'Bootstrap  ' },
+            { label: 'Materializar  ' },
+            { label: 'Fundação ' },
+           ],
+          },
+        ],
+      },
+      {
+        label: 'JavaScript básico',
+        children: [
+          {
+            label: 'Variáveis e tipos de dados',
+            children: [
+              { label: 'variáveis ',
+              children:[
+                {label:'var '},
+              ] },
+              { label: 'Operadores ',
+              children:[
+                {label:'Aritméticos '},
+                {label:'Lógicos  '},
+              ] },
+              { label: 'String  ',
+              children:[
+                {label:'Concatenando string'},
+                {label:'Expressões regulares'},
+              ] },
+            ],
+          },
+          {
+            label: 'Estruturas de controle',
+            children: [
+              { label: 'condicionais',
+              children:[
+                {label:'if'},
+                {label:'else'},
+                {label:'else if'},
+                {label:'switch'},
+              ] },
+              { label: 'loops',
+              children:[
+                {label:'For'},
+                {label:'while'},
+                {label:'do...while'},
+              ] },
+            ],
+          },
+          {
+            label: 'Funções ',
+            children: [
+              {label: 'Declarações de funções'},
+              {label: 'Expressões de funções' },
+              {label: 'Escopo de variáveis' },
+              {label: 'Hoisting ' },
+            ],
+          },
+          {
+            label: 'Criando Array',
+            children: [
+              { label: 'Métodos array' },
+              { label: 'Manipulação de arrays'},
+              { label: 'Ordenação' },
+              { label: 'filtragem de arrays' },
+              { label: 'Iteração sobre arrays' },
+            ],
+          },
+          {
+            label: 'Manipulação do DOM',
+            children: [
+              { label: 'Selecionando elementos do DOM' },
+              { label: 'Métodos de manipulação de elementos DOM',
+              children:[
+                {label:'innerHTML'},
+                {label:'innertext'},
+                {label:'innerContent'},
+                {label:'outerHTML'},
+                {label:'style'},
+                {label:'Criar elementios'},
+                {label:'Clonar elementos'},
+                {label:'Remover elementos'},
+                {label:'class classList'},
+                {label:'getComputedStyle'},
+                {label:'Dimensões e localizações'},
+              ] },
+              { label: 'Class e DOM'},
+            ],
+          },
+          {
+            label: 'Eventos do DOM',
+            children: [
+              { label: 'Adicionando eventos',
+              children:[
+                {label:'On+NomeDoEvento'},
+              ] },
+              { label: 'Utilizando ouvidores',
+              children:[
+                {label:'addEventListener'},
+                {label:'removeEventListener'},
+              ] },
+              { label: 'Evento de clique',
+              children:[
+                {label:'click event'},
+                {label:'dblclick event'},
+              ] },
+              { label: 'Eventos de mouse',
+              children:[
+                {label:'mousemove event'},
+                {label:'mouseenter event'},
+                {label:'mouseleave event'},
+                {label:'mouseover event'},
+                {label:'mouseout event'},
+              ] },
+              { label: 'Eventos de teclado',
+              children:[
+                {label:'keydown event'},
+                {label:'keypress event'},
+                {label:'keyup event'},
+              ] },
+              { label: 'Eventos de formulário',
+              children:[
+                {label:'submit event'},
+                {label:'change event'},
+                {label:'focus event'},
+                {label:'blur event'},
+              ] },
+              { label: 'Eventos de tempo',
+              children:[
+                {label:'setTimeout'},
+                {label:'setInterval'},
+              ] },
+              { label: 'Eventos de redimensionamento e rolagem',
+              children:[
+                {label:'resize event'},
+                {label:'scroll event'},
+              ] },
+              { label: 'Eventos de toque (para dispositivos móveis)',
+              children:[
+                {label:'touchstart event'},
+                {label:'touchmove event'},
+                {label:'touchend event'},
+              ] },
+              { label: 'Eventos de arrastar e soltar (drag and drop)',
+              children:[
+                {label:'drag event'},
+                {label:'dragenter event'},
+                {label:'dragleave event'},
+                {label:'dragover event'},
+                {label:'drop event'},
+              ] },
+              { label: 'Delegação de eventos',
+              children:[
+                {label:'Conceito de delegação de eventos'},
+                {label:'Benefícios da delegação de eventos'},
+              ] },
+              { label: 'Eventos personalizados',
+              children:[
+                {label:'Criando eventos personalizados com CustomEvent'},
+                {label:'Disparando eventos personalizados com dispatchEvent'},
+              ] },
+            ],
+          },
+          {
+            label: 'Criando objetos',
+            children: [
+              { label: 'Métodos'},
+              { label: 'Propriedades de objetos'},
+              { label: 'Protótipos'},
+              { label: 'Herança'},
+              { label: 'Classes'},
+            ],
+          },
+          {
+            label: 'Trabalhando com funções avançadas',
+            children: [
+              { label: 'Closures'},
+              { label: 'Callbacks'},
+            ],
+          },
+          {
+            label: 'Objeto Date',
+            children: [
+              { label: 'Métodos de data'},
+              { label: 'Moment.js (biblioteca de manipulação de datas)'},
+            ],
+          },
+          {
+            label: 'Trabalhando com json',
+            children: [
+              { label: 'Criando json'},
+              { label: 'Convertendo json para objeto'},
+              { label: 'Importação de arquivo json'},
+            ],
+          },
+          {
+            label: 'Trabalhando com AJAX',
+            children: [
+              { label: 'Introdução ao AJAX'},
+              { label: 'XMLHttpRequest '},
+              { label: 'Fetch API'},
+              { label: 'Armazenamento de dados'},
+            ],
+          },
+          {
+            label: 'Cookies ',
+            children: [
+              { label: 'LocalStorage e SessionStorage'},
+              { label: 'IndexedDB'},
+            ],
+          },
+        ],
+      },
+      {
+        label: 'JavaScript/ES6',
+        children: [
+          {
+            label: 'Variáveis e escopo',
+            children: [
+              { label: 'Declaração de variáveis',
+              children:[
+                {label:'let'},
+                {label:'const'},
+              ] },
+              { label: 'Blocos de escopo  '},
+              { label: 'Escopo de variáveis em loops '},
+            ],
+          },
+          {
+            label: 'Arrow functions',
+            children: [
+              { label: 'condicionais',
+              children:[
+                {label:'if'},
+                {label:'else'},
+                {label:'else if'},
+                {label:'switch'},
+              ] },
+              { label: 'loops',
+              children:[
+                {label:'For'},
+                {label:'while'},
+                {label:'do...while'},
+              ] },
+            ],
+          },
+          {
+            label: 'Funções ',
+            children: [
+              {label: 'Declarações de funções'},
+              {label: 'Expressões de funções' },
+              {label: 'Escopo de variáveis' },
+              {label: 'Hoisting ' },
+            ],
+          },
+          {
+            label: 'Criando Array',
+            children: [
+              { label: 'Métodos array' },
+              { label: 'Manipulação de arrays'},
+              { label: 'Ordenação' },
+              { label: 'filtragem de arrays' },
+              { label: 'Iteração sobre arrays' },
+            ],
+          },
+          {
+            label: 'Manipulação do DOM',
+            children: [
+              { label: 'Selecionando elementos do DOM' },
+              { label: 'Métodos de manipulação de elementos DOM',
+              children:[
+                {label:'innerHTML'},
+                {label:'innertext'},
+                {label:'innerContent'},
+                {label:'outerHTML'},
+                {label:'style'},
+                {label:'Criar elementios'},
+                {label:'Clonar elementos'},
+                {label:'Remover elementos'},
+                {label:'class classList'},
+                {label:'getComputedStyle'},
+                {label:'Dimensões e localizações'},
+              ] },
+              { label: 'Class e DOM'},
+            ],
+          },
+          {
+            label: 'Eventos do DOM',
+            children: [
+              { label: 'Adicionando eventos',
+              children:[
+                {label:'On+NomeDoEvento'},
+              ] },
+              { label: 'Utilizando ouvidores',
+              children:[
+                {label:'addEventListener'},
+                {label:'removeEventListener'},
+              ] },
+              { label: 'Evento de clique',
+              children:[
+                {label:'click event'},
+                {label:'dblclick event'},
+              ] },
+              { label: 'Eventos de mouse',
+              children:[
+                {label:'mousemove event'},
+                {label:'mouseenter event'},
+                {label:'mouseleave event'},
+                {label:'mouseover event'},
+                {label:'mouseout event'},
+              ] },
+              { label: 'Eventos de teclado',
+              children:[
+                {label:'keydown event'},
+                {label:'keypress event'},
+                {label:'keyup event'},
+              ] },
+              { label: 'Eventos de formulário',
+              children:[
+                {label:'submit event'},
+                {label:'change event'},
+                {label:'focus event'},
+                {label:'blur event'},
+              ] },
+              { label: 'Eventos de tempo',
+              children:[
+                {label:'setTimeout'},
+                {label:'setInterval'},
+              ] },
+              { label: 'Eventos de redimensionamento e rolagem',
+              children:[
+                {label:'resize event'},
+                {label:'scroll event'},
+              ] },
+              { label: 'Eventos de toque (para dispositivos móveis)',
+              children:[
+                {label:'touchstart event'},
+                {label:'touchmove event'},
+                {label:'touchend event'},
+              ] },
+              { label: 'Eventos de arrastar e soltar (drag and drop)',
+              children:[
+                {label:'drag event'},
+                {label:'dragenter event'},
+                {label:'dragleave event'},
+                {label:'dragover event'},
+                {label:'drop event'},
+              ] },
+              { label: 'Delegação de eventos',
+              children:[
+                {label:'Conceito de delegação de eventos'},
+                {label:'Benefícios da delegação de eventos'},
+              ] },
+              { label: 'Eventos personalizados',
+              children:[
+                {label:'Criando eventos personalizados com CustomEvent'},
+                {label:'Disparando eventos personalizados com dispatchEvent'},
+              ] },
+            ],
+          },
+          {
+            label: 'Criando objetos',
+            children: [
+              { label: 'Métodos'},
+              { label: 'Propriedades de objetos'},
+              { label: 'Protótipos'},
+              { label: 'Herança'},
+              { label: 'Classes'},
+            ],
+          },
+          {
+            label: 'Trabalhando com funções avançadas',
+            children: [
+              { label: 'Closures'},
+              { label: 'Callbacks'},
+            ],
+          },
+          {
+            label: 'Objeto Date',
+            children: [
+              { label: 'Métodos de data'},
+              { label: 'Moment.js (biblioteca de manipulação de datas)'},
+            ],
+          },
+          {
+            label: 'Trabalhando com json',
+            children: [
+              { label: 'Criando json'},
+              { label: 'Convertendo json para objeto'},
+              { label: 'Importação de arquivo json'},
+            ],
+          },
+          {
+            label: 'Trabalhando com AJAX',
+            children: [
+              { label: 'Introdução ao AJAX'},
+              { label: 'XMLHttpRequest '},
+              { label: 'Fetch API'},
+              { label: 'Armazenamento de dados'},
+            ],
+          },
+          {
+            label: 'Cookies ',
+            children: [
+              { label: 'LocalStorage e SessionStorage'},
+              { label: 'IndexedDB'},
+            ],
+          },
+        ],
+      },
     ],
   },
 ];
